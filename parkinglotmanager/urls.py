b3fc0dc29115f06b2a19769ff27c3d408e937704
john <nguyenhongky1998@gmail.com>
2020-10-10 23:12:32 +0700

    urls to viewset

---
+++ parkinglotmanager/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from .views import ParkingLotList, ParkingList, \
+    ParkingUpdate, ParkingDelete, ParkingCreate, Task_Delete, \
+    LotDetail
+# ParkingDetail,
+app_name = "alpr"
+urlpatterns = [
+    path('lotlist/', ParkingLotList.as_view(), name="list-all"),
+    path('lotdetail/<str:pk>/', LotDetail.as_view(), name="list-detail"),
+    path('list/', ParkingList.as_view(), name="parking-list"),
+    path('update/<str:pk>/', ParkingUpdate.as_view(), name="parking-update"),
+    path('delete/<str:pk>/', ParkingDelete.as_view(), name="parking-delete"),
+    path('del/<str:pk>/', Task_Delete, name="del"),
+    path('new/', ParkingCreate.as_view(), name="parking-create"),
+
+
+
+
+    #path('detail/<str:pk>/', ParkingDetail.as_view(), name="parking-detail"),
+]
