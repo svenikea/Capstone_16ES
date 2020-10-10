98f086af7c7d731d0cf715d76fa98779edd41d8e
john <nguyenhongky1998@gmail.com>
2020-10-10 23:13:14 +0700

    new changes

--- parkinglotmanager/models.py
+++ parkinglotmanager/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.urls import reverse
 
 
 class AuthGroup(models.Model):
@@ -157,7 +158,7 @@ class History(models.Model):
 class Parking(models.Model):
     rfid = models.CharField(primary_key=True, max_length=12)
     parkinglotid = models.ForeignKey(
-        'Parkinglotlist', models.DO_NOTHING, db_column='parkinglotid')
+        'Parkinglotlist', on_delete=models.CASCADE, db_column='parkinglotid', related_name='ParkingID')
     platenumber = models.CharField(max_length=15, blank=True, null=True)
     plateimgurl = models.CharField(max_length=255, blank=True, null=True)
     checkintime = models.DateTimeField(blank=True, null=True)
@@ -169,6 +170,9 @@ class Parking(models.Model):
 
     def __str__(self):
         return self.rfid
+
+    def get_absolute_url(self):
+        return reverse('alpr:parking-detail', kwargs={"pk": self.pk})
 
 
 class Parkinglotlist(models.Model):
