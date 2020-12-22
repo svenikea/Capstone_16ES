9a6b1660eb8f18396193484495c34a69124e5cd2
john <nguyenhongky1998@gmail.com>
2020-12-22 23:17:57 +0700

    added staff table to admin page

--- parkinglotmanager/admin.py
+++ parkinglotmanager/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Parking, Parkinglotlist
+from .models import Parking, Parkinglotlist, Stafflist
 # Register your models here.
 admin.site.register(Parking)
 admin.site.register(Parkinglotlist)
+admin.site.register(Stafflist)
