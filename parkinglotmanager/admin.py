9b2cd568225c00b56ff255ee2af42c5b9b29a844
nguyen.hong.ky <SUN-ASTERISK\nguyen.hong.ky@b122344-lt.sun-asterisk.com>
2020-12-23 15:57:45 +0700

    added new history and camera model

--- parkinglotmanager/admin.py
+++ parkinglotmanager/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Parking, Parkinglotlist, Stafflist
+from .models import Parking, Parkinglotlist, Stafflist, History, \
+        Cameralist
 # Register your models here.
 admin.site.register(Parking)
 admin.site.register(Parkinglotlist)
 admin.site.register(Stafflist)
+admin.site.register(History)
+admin.site.register(Cameralist)
