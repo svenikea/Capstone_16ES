f04ef85367513fa537774bea94ac1fe130cd7abb
john <nguyenhongky1998@gmail.com>
2020-12-27 21:59:05 +0700

    changed the path of the project for apache webserver

--- capstone/wsgi.py
+++ capstone/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 sys.path.append('/home/john/Documents/project/Capstone_16ES')
