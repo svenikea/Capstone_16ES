e439e208584ed83a5270ce48d9f8fecfab04d20d
nguyen.hong.ky <SUN-ASTERISK\nguyen.hong.ky@b122344-pc.sun-asterisk.com>
2021-04-20 11:42:28 +0700

    updated script

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
-source /.cp/bin/activate
-./manage.py makemigrations
-./manage.py migrate
+/.env/bin/python ./manage.py makemigrations
+/.env/bin/python ./manage.py migrate
 #/.cp/bin/python ./manage.py shell -c "from django.contrib.auth.models import User; \
  #                          User.objects.create_superuser('john','', '123')"
-./manage.py shell -c "from django.contrib.auth.models import User; \
+/.env/bin/python ./manage.py shell -c "from django.contrib.auth.models import User; \
                            User.objects.filter(username='john').exists() or \
                            User.objects.create_superuser('john',
                            '', '123')" 
-./manage.py runserver 172.18.0.4:80
+/.env/bin/python./manage.py runserver 0.0.0.0:80
