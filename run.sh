ef43118691fc96b5627581c7e8668de0507126b4
nguyen.hong.ky <SUN-ASTERISK\nguyen.hong.ky@b122344-pc.sun-asterisk.com>
2021-05-07 09:48:19 +0700

    change django admin username/password

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #/.cp/bin/python ./manage.py shell -c "from django.contrib.auth.models import User; \
  #                          User.objects.create_superuser('john','', '123')"
 /.env/bin/python ./manage.py shell -c "from django.contrib.auth.models import User; \
-                           User.objects.filter(username='john').exists() or \
-                           User.objects.create_superuser('john',
+                           User.objects.filter(username='test').exists() or \
+                           User.objects.create_superuser('test',
                            '', '123')" 
-/.env/bin/python./manage.py runserver 0.0.0.0:80
+/.env/bin/python./manage.py runserver 0.0.0.0:8000
